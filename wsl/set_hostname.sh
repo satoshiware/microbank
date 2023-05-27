f1962795ba5b27bad2b82bf099a23a59f2232594
satoshiware <satoshiware@gmail.com>
2023-05-26 19:16:30 -0700

    Update set_hostname.sh

--- wsl/set_hostname.sh
+++ wsl/set_hostname.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# Generate WSL configuration and update the hostname
+# Get the desired hostname from the user
 read -p "Enter the desired hostname: "
+
+# Generate WSL configuration
 echo "[network]" | sudo tee /etc/wsl.conf
 echo "hostname = $REPLY" | sudo tee -a /etc/wsl.conf
 echo "generateHosts = false" | sudo tee -a /etc/wsl.conf
 echo "[boot]" | sudo tee -a /etc/wsl.conf
 echo "systemd=true" | sudo tee -a /etc/wsl.conf
-echo "[user]" | sudo tee -a /etc/wsl.conf
-echo "default=$USER" | sudo tee -a /etc/wsl.conf
 
 # Update hostname in hosts file
 sudo sed -i "s/$(hostname)/${REPLY}/g" /etc/hosts
+
+# Get the default username from the user
+read -p "Enter the default username: "
+echo "[user]" | sudo tee -a /etc/wsl.conf
+echo "default=${REPLY}" | sudo tee -a /etc/wsl.conf
+
+# User Reminder
+clear
+echo "Don\'t forget to exit to powershell and restart this instance"
+echo "wsl --unregister \$INSTANCE"
