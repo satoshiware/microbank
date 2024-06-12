f8fb083d49666ad1600c0f452e37b67271b86c6f
Satoshiware <satoshiware@gmail.com>
2024-06-12 07:15:08 -0700

    Update stmutility.sh

--- scripts/pre_fork_micro/stmutility.sh
+++ scripts/pre_fork_micro/stmutility.sh
@@ -108,3 +108,45 @@ elif [[ $1 = "-t" || $1 = "--tail" ]]; then # Display the tail end of the debug 
 else
     $0 --help
 fi
+
+
+
+
+
+    sudo sed -i "/${2}/d" /root/.ssh/known_hosts 2> /dev/null # Remove the known host containing the time stamp
+	
+	
+	
+
+elif [[ $1 = "-g" || $1 = "--generate" ]]; then # Generate micronode information file (/etc/micronode.info) with connection parameters for this node
+    if [ -f "/etc/micronode.info" ]; then
+        echo "/etc/micronode.info file already exists"
+        exit 0
+    fi
+
+    echo "Here's important information about your micronode." | sudo tee /etc/micronode.info > /dev/null
+    echo "It can be used to establish secure micronode connections over ssh." | sudo tee -a /etc/micronode.info > /dev/null
+    echo "" | sudo tee -a /etc/micronode.info
+
+    echo "Hostname: $(hostname)" | sudo tee -a /etc/micronode.info
+    echo "Time Stamp: $(date +%s)" | sudo tee -a /etc/micronode.info
+
+    echo "Local IP: $(hostname -I)" | sudo tee -a /etc/micronode.info
+
+    if [ -z ${SSHPORT+x} ]; then SSHPORT="22"; fi
+    echo "SSH Port: ${SSHPORT}" | sudo tee -a /etc/micronode.info
+
+    # Remove unwanted/unused host keys
+    sudo rm /etc/ssh/ssh_host_dsa_key* 2> /dev/null
+    sudo rm /etc/ssh/ssh_host_ecdsa_key* 2> /dev/null
+    sudo rm /etc/ssh/ssh_host_rsa_key* 2> /dev/null
+
+    echo "Host Key (Public): $(sudo cat /etc/ssh/ssh_host_ed25519_key.pub | sed 's/ root@.*//')" | sudo tee -a /etc/micronode.info
+    echo "P2P Key (Public): $(sudo cat /root/.ssh/p2pkey.pub)" | sudo tee -a /etc/micronode.info
+
+    sudo chmod 400 /etc/micronode.info
+else
+    $0 --help
+fi
+
+
