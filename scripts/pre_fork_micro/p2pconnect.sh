d14f8891b1adaf2b9d8d29b3a413079617f75a61
satoshiware <satoshiware@gmail.com>
2024-07-09 13:50:09 -0700

    Update p2pconnect.sh

--- scripts/pre_fork_micro/p2pconnect.sh
+++ scripts/pre_fork_micro/p2pconnect.sh
@@ -18,13 +18,13 @@ if [[ $1 = "-h" || $1 = "--help" ]]; then # Show all possible paramters
     Options:
       -h, --help        Display this help message and exit
       --install         Install (or upgrade) this script (p2pconnect) in /usr/local/sbin/ (/satoshiware/microbank/scripts/pre_fork_micro/p2pconnect.sh)
-!!!!!why "/bin/bash -ls"!!!!!	  --cron            Install (or upgrade) this script (p2pconnect) in /usr/local/sbin/ (/satoshiware/microbank/scripts/pre_fork_micro/p2pconnect.sh)
-	  --verify		    Verify all connections (in and out) are active: RECIPIENTS_NAME  EMAIL
-							send email of there are any inactive/disconnected nodes (requires send_messages to be configured)
-							If any inbound (non-cluster nodes) connections have become inactive, run a "Dynamic DNS" script
-	  -n, --in          Configure inbound cluster connection (p2p <-- wallet, p2p <-- stratum, or p2p <-- electrum)
+!!!!!why "/bin/bash -ls"!!!!!     --cron            Install (or upgrade) this script (p2pconnect) in /usr/local/sbin/ (/satoshiware/microbank/scripts/pre_fork_micro/p2pconnect.sh)
+      --verify          Verify all connections (in and out) are active: RECIPIENTS_NAME  EMAIL
+                            send email if there are any inactive/disconnected nodes (requires send_messages to be configured)
+                            Note: If any inbound (non-cluster nodes) connections have become inactive, they "Dynamic DNS" script will be called
+      -n, --in          Configure inbound cluster connection (p2p <-- wallet, p2p <-- stratum, or p2p <-- electrum)
       -p, --p2p         Make p2p inbound/outbound connections (p2p <--> p2p)
-      -v, --view        See all configured connections and status
+      -v, --view        See all configured connections and statuses
       -d, --delete      Delete a connection: CONNECTION_ID
       -f, --info        Get the connection parameters for this node
 EOF
@@ -104,8 +104,26 @@ EOF
     echo "addnode=localhost:${LOCALMICROPORT}" | sudo tee -a /etc/bitcoin.conf # Add connection
 
 elif [[ $1 = "-v" || $1 = "--view" ]]; then # See all configured connections and status
-    # Show the contents of the Bitcoin configuration file
-    echo ""; echo "Outbound: Bitcoin Configuration File (/etc/bitcoin.conf)"; echo "------------------------------------------------------------------"; sudo cat /etc/bitcoin.conf
+    # Show network info
+    networkinfo=$(btc getnetworkinfo)
+    echo "RPC \"getnetworkinfo\":"
+    echo -n "    Network Active: "; echo $networkinfo | jq -r '.networkactive'
+    echo -n "    In Connection(s): "; echo $networkinfo | jq -r '.connections_in'
+    echo -n "    Out Connection(s): "; echo $networkinfo | jq -r '.connections_out'
+    echo -n "    Total Connection(s): "; echo $networkinfo | jq -r '.connections'
+
+    # Show peer info
+    echo ""; echo "RPC \"getpeerinfo\":"
+    peerinfo=$(btc getpeerinfo)
+    count=$(echo $peerinfo | jq -r '. | length')
+    for (( i=0; i<$count; i++ )); do
+        echo "    Connection: $((i+1))"
+        echo -n "    Address: "; echo $peerinfo | jq -r ".[$i].addr"
+        echo -n "    Connection Type: "; echo $peerinfo | jq -r ".[$i].connection_type"; echo ""
+    done
+
+    # Show the contents of the Authorized Keys file (inbound)
+    echo ""; echo "Inbound: Authorized Keys File (/home/p2p/.ssh/authorized_keys)"; echo "------------------------------------------------------------------"; sudo cat /home/p2p/.ssh/authorized_keys
 
     # Loop through each added node (i.e. local port)
     echo ""; echo "Outbound: Added Node Info (Auto SSH Forwarded Ports)"; echo "------------------------------------------------------------------"
@@ -114,11 +132,11 @@ elif [[ $1 = "-v" || $1 = "--view" ]]; then # See all configured connections and
         echo -n $info | jq -j -r ".[$i].addednode"; echo -n "        Connected: "; echo $info | jq ".[$i].connected"
     done
 
+    # Show the contents of the Bitcoin configuration file
+    echo ""; echo "Outbound: Bitcoin Configuration File (/etc/bitcoin.conf)"; echo "------------------------------------------------------------------"; sudo cat /etc/bitcoin.conf
+
     # Show the contents of the Known Hosts file (outbound)
     echo ""; echo "Outbound: Known Hosts File (/root/.ssh/known_hosts)"; echo "------------------------------------------------------------------"; sudo cat /root/.ssh/known_hosts
-
-    # Show the contents of the Authorized Keys file (inbound)
-    echo ""; echo "Inbound: Authorized Keys File (/home/p2p/.ssh/authorized_keys)"; echo "------------------------------------------------------------------"; sudo cat /home/p2p/.ssh/authorized_keys
 
     # Show the p2pssh (autossh) process for each outgoing connection
     echo ""; echo "Outbound: View each p2pssh@* .env file and process status (/etc/default/p2pssh@*)"; echo "------------------------------------------------------------------"
@@ -127,37 +145,6 @@ elif [[ $1 = "-v" || $1 = "--view" ]]; then # See all configured connections and
         echo "#########$(sudo head -n 1 $i)    $(sudo sed '2!d' $i)    $(sudo sed '3!d' $i)    $(sudo tail -n 1 $i) ########"
         sudo systemctl status $(echo $i | cut -d "/" -f 4); echo ""
     done
-
-
-
-elif [[ $1 = "-dd" || $1 = "--disconnected" ]]; then # Find all disconnections !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	# Make sure all added nodes are connected
-    info=$(btc getaddednodeinfo); length=$(echo -n $info | jq length)
-    for (( i=0; i<$length; i++ )); do
-        if [[ $(echo -n $info | jq ".[$i].connected") == "false" ]]; then
-			echo "Oh No! Node \"$(echo $info | jq -j -r ".[$i].addednode")\" is disconnected!"
-		fi
-    done
-
-
-    # Search in stratum|p2p authorized_keys to match the timestamp with the . You know, we could just disconnect everyone!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-#   ss -t -a | grep ssh
-#   ipaddr="192.168.1.240"
-#   ddport="42682"
-#   sdport="42676"
-
-#   cat /var/log/auth.log | grep "Accepted publickey for.*${ipaddr}.*${ddport}" # what about the stratum user instead of p2p
-#   UfullVXA44hjjgvle9qIP3Hn5vjrnj5vTq0nT5Z2Y2M
-
-#    AAC3NzaC1lZDI1NTE5AAAAIMZ0yYY38wDVbwxjjeWY+sGQUrHkMIthSRgAOVdAA+Z4
-#    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ0yYY38wDVbwxjjeWY+sGQUrHkMIthSRgAOVdAA+Z4
-
-#   cd ~  ###### try the "<<<" redirector instead
-#   mkfifo fifo
-#   echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ0yYY38wDVbwxjjeWY+sGQUrHkMIthSRgAOVdAA+Z4" > fifo &
-#   okok=$(ssh-keygen -l -f fifo)
-#   rm fifo
 
 elif [[ $1 = "-d" || $1 = "--delete" ]]; then # Delete a connection: CONNECTION_ID
     if [[ ! ${#} = "2" ]]; then
@@ -193,10 +180,46 @@ elif [[ $1 = "-f" || $1 = "--info" ]]; then # Get the connection parameters for 
     echo "Hostname: $(hostname)" | sudo tee -a /etc/micronode.info
     echo "Local IP: $(hostname -I)" | sudo tee -a /etc/micronode.info
 
-    echo "Host Key (Public): $(sudo cat /etc/ssh/ssh_host_ed25519_key.pub | sed 's/ root@.*//')"
-    echo "P2P Key (Public): $(sudo cat /root/.ssh/p2pkey.pub)"
+    echo "This Host Key (Public): $(sudo cat /etc/ssh/ssh_host_ed25519_key.pub | sed 's/ root@.*//')"
+    echo "This P2P Key (Public): $(sudo cat /root/.ssh/p2pkey.pub)"
 
 else
     $0 --help
     echo "Script Version 0.16"
 fi
+
+
+
+
+#elif [[ $1 = "-dd" || $1 = "--disconnected" ]]; then # Find all disconnections !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Make sure all added nodes are connected
+#    info=$(btc getaddednodeinfo); length=$(echo -n $info | jq length)
+#    for (( i=0; i<$length; i++ )); do
+#        if [[ $(echo -n $info | jq ".[$i].connected") == "false" ]]; then
+#           echo "Oh No! Node \"$(echo $info | jq -j -r ".[$i].addednode")\" is disconnected!"
+#       fi
+#    done
+
+
+
+
+
+
+    # Search in stratum|p2p authorized_keys to match the timestamp with the . You know, we could just disconnect everyone!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+#   ss -t -a | grep ssh
+#   ipaddr="192.168.1.240"
+#   ddport="42682"
+#   sdport="42676"
+
+#   cat /var/log/auth.log | grep "Accepted publickey for.*${ipaddr}.*${ddport}" # what about the stratum user instead of p2p
+#   UfullVXA44hjjgvle9qIP3Hn5vjrnj5vTq0nT5Z2Y2M
+
+#    AAC3NzaC1lZDI1NTE5AAAAIMZ0yYY38wDVbwxjjeWY+sGQUrHkMIthSRgAOVdAA+Z4
+#    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ0yYY38wDVbwxjjeWY+sGQUrHkMIthSRgAOVdAA+Z4
+
+#   cd ~  ###### try the "<<<" redirector instead
+#   mkfifo fifo
+#   echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ0yYY38wDVbwxjjeWY+sGQUrHkMIthSRgAOVdAA+Z4" > fifo &
+#   okok=$(ssh-keygen -l -f fifo)
+#   rm fifo
