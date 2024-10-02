93e9c52daa021d4ff00c9711450ed6cb6e6c0a93
Satoshiware <satoshiware@gmail.com>
2024-10-02 15:37:00 -0700

    Update base_setup.sh

--- scripts/base_setup.sh
+++ scripts/base_setup.sh
@@ -81,6 +81,7 @@ sudo apt-get -y install guestfs-tools           # Provides a set of extended com
 sudo apt-get -y install libosinfo-bin           # A library for managing OS information for virtualization. (libosinfo)
 sudo apt-get -y install tuned                   # A system tuning service for Linux.
 sudo apt-get -y install jq                      # Command-line JSON processing tool
+sudo apt-get -y install rsync                   # Remote (& local) file synchronization tool. The host coordinates the backing up of important files for each instance.
 
 # Disable Password Authentication
 sudo sed -i 's/#.*PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config # Disable password login
